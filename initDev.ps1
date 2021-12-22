d9ccbc5b1206e382eb57e4f21bb167bbcb96d0f3
ziyuan <hanziyuan08@outlook.com>
2021-12-22 11:50:10 +0800

    rename bucket

--- initDev.ps1
+++ initDev.ps1
@@ -2,7 +2,7 @@ Write-Output "Installing Git"
 scoop install git
 
 scoop bucket add dorado https://github.com/chawyehsu/dorado
-scoop bucket add forkedMain https://github.com/Bpazy/Main
+scoop bucket add bpazyForked https://github.com/Bpazy/Main
 
 Write-Output "Installing Busybox"
 scoop install busybox
@@ -12,7 +12,7 @@ scoop install aria2
 scoop config aria2-enabled true
 
 Write-Output "Intalling Go..."
-scoop install forkedMain/go 
+scoop install bpazyForked/go 
 
 Write-Output "Installing sudo..."
 scoop install sudo 
@@ -23,11 +23,17 @@ scoop install openjdk11
 Write-Output "Installing openjdk8-redhet..."
 scoop bucket add java
 scoop install openjdk8-redhat
+scoop reset openjdk8-redhat
 
 Write-Output "Use scoop reset <java>[@<version>] to switch Javas"
 
+Write-Output "Installing python27"
+scoop bucket add versions
+scoop install python27
+
 Write-Output "Installing Python"
 scoop install python
+scoop reset python
 
 Write-Output "Installing Nodejs"
 scoop install nodejs
@@ -39,10 +45,6 @@ scoop install make
 
 Write-Output "Installing VIM"
 scoop install vim
-
-Write-Output "Installing python27"
-scoop bucket add versions
-scoop install python27
 
 Write-Output "Installing groovy"
 scoop install groovy
